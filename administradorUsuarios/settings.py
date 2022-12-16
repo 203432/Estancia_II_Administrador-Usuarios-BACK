a99e7bc90f1f825d6726033eb198a49b5b9cf199
Jorge Camacho <203432@ids.upchiapas.edu.mx>
2022-12-16 09:37:24 -0600

    add photo on users

--- administradorUsuarios/settings.py
+++ administradorUsuarios/settings.py
@@ -150,6 +150,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = '/assets/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
